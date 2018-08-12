1480522d3b6d5f73c2f3981e401e179cf029d68d
Rob Dyson <rdyson@hotmail.com>
2018-08-12 17:13:28 -0400

    Sorting done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import Restaurant from './components/Restaurant';
 import Geolocation from './components/Geolocation';
-import geolib from 'geolib';
 import './App.css';
+import geolib from 'geolib';
+import { geolocated } from 'react-geolocated';
 import base from './base';
 
 class App extends Component {
   state = {
     restaurants: {},
     reviews: {},
+    restaurantsNearby: {},
     lat: '',
     lng: ''
   };
@@ -22,6 +24,37 @@ class App extends Component {
       context: this,
       state: 'restaurants'
     });
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    geolocated({
+      positionOptions: {
+        enableHighAccuracy: false
+      },
+      userDecisionTimeout: 5000
+    })(Geolocation);
+
+    const lat = this.state.lat;
+    const lng = this.state.lng;
+
+    if (previousState.restaurants !== this.state.restaurants) {
+      const restaurantsNearby = [...this.state.restaurants]
+        .filter(key => key)
+        .map(function(el) {
+          var object = Object.assign({}, el);
+          object.distance = geolib.getDistanceSimple(
+            { latitude: lat, longitude: lng },
+            { latitude: el.lat, longitude: el.lng }
+          );
+          return object;
+        })
+        .sort(function(a, b) {
+          return a.distance - b.distance;
+        });
+      this.setState({
+        restaurantsNearby: restaurantsNearby
+      });
+    }
   }
 
   getReviews(restaurantId) {
@@ -44,10 +77,10 @@ class App extends Component {
     return (
       <div>
         <Geolocation setLocation={this.setLocation} />
-        {Object.keys(this.state.restaurants).map(key => (
+        {Object.keys(this.state.restaurantsNearby).map(key => (
           <Restaurant
             key={key}
-            details={this.state.restaurants[key]}
+            details={this.state.restaurantsNearby[key]}
             reviews={this.getReviews(key)}
             currentLat={this.state.lat}
             currentLng={this.state.lng}
--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -3,6 +3,24 @@ import Review from './Review';
 import geolib from 'geolib';
 
 class Restaurant extends Component {
+  componentDidMount() {
+    if (
+      this.props.currentLat &&
+      this.props.currentLng &&
+      this.props.details.lat &&
+      this.props.details.lng
+    ) {
+      const currentLat = this.props.currentLat;
+      const currentLng = this.props.currentLng;
+      const { lat, lng } = this.props.details;
+      this.props.details.distanceInComponent =
+        geolib.getDistanceSimple(
+          { latitude: lat, longitude: lng },
+          { latitude: currentLat, longitude: currentLng }
+        ) / 1000;
+    }
+  }
+
   render() {
     const { name, address, price, lat, lng } = this.props.details;
     const currentLat = this.props.currentLat;
@@ -15,12 +33,13 @@ class Restaurant extends Component {
         </h2>
         <p>
           {address} (
-          {Math.round(
-            geolib.getDistanceSimple(
-              { latitude: lat, longitude: lng },
-              { latitude: currentLat, longitude: currentLng }
-            ) / 1000
-          )}
+          {this.props.currentLat &&
+            Math.round(
+              geolib.getDistanceSimple(
+                { latitude: lat, longitude: lng },
+                { latitude: currentLat, longitude: currentLng }
+              ) / 1000
+            )}
           km away)
         </p>
 
