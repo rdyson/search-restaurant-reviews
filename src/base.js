1c1cd74b893e161678987c70dee31b36213fca2d
Rob Dyson <rdyson@hotmail.com>
2019-03-11 22:38:54 -0400

    Updated database details

--- src/base.js
+++ src/base.js
@@ -2,9 +2,9 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: 'AIzaSyBdqKe4r3SVKUjNhXWdBGOmtj9j5872J98',
-  authDomain: 'search-yelp-reviews.firebaseapp.com',
-  databaseURL: 'https://search-yelp-reviews.firebaseio.com'
+  apiKey: 'AIzaSyCXnrKTv53uKEi2hzPdK93GrNC0r6MbOZM',
+  authDomain: 'vegan-restaurants-nearby.firebaseapp.com',
+  databaseURL: 'https://vegan-restaurants-nearby.firebaseio.com/'
 });
 
 const base = Rebase.createClass(firebaseApp.database());
